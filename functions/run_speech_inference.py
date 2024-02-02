e39a6e3fac920d3f7a3f1676646720674255c8ee
314Reactor <34923578+LordofBone@users.noreply.github.com>
2024-02-02 23:36:08 +0000

    Added ability to delay init of models

--- functions/run_speech_inference.py
+++ functions/run_speech_inference.py
@@ -11,7 +11,7 @@ logger = logging.getLogger(__name__)
 
 
 class SpeechInference:
-    def __init__(self, audio_file, offline_mode, model_size, api_key):
+    def __init__(self, audio_file, offline_mode, model_size, api_key, init_on_launch=True):
         """
         Initialize the speech inference class.
         """
@@ -20,16 +20,24 @@ class SpeechInference:
 
         self.offline_mode = offline_mode
 
+        # this is for code calling this to have the option to not initialize the STT model until later or on init of
+        # the class
+        if init_on_launch:
+            self.init_models()
+
+    def init_models(self):
+        """
+        Initialize the models.
+        :return:
+        """
         if self.offline_mode:
             logger.debug("Initializing Speech Inference model")
-            self.model = whisper.load_model(model_size)
+            self.model = whisper.load_model(self.model_size)
             logger.debug("Initialized Speech Inference model")
         else:
             logger.debug("Initializing OpenAI API")
-            openai.api_key = api_key
+            openai.api_key = self.api_key
             logger.debug("Initialized OpenAI API")
-
-        logger.debug("Initialized")
 
     def run_stt(self):
         """
